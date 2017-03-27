b55c117ef587f0f46ea2398ed3cd6c9239413c96
huntertaylor <zotytaylor@gmail.com>
2017-03-27 21:26:41 +0200

    Init Commit

---
+++ ACTIServer/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.MemoryMappedFiles;
+using System.Threading;
+using System.Diagnostics;
+using ACTIServer.Structs;
+using Newtonsoft.Json;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ACTIServer
+{
+    public partial class Form1 : Form
+    {
+        private Boolean fut = true;
+        private Thread thread;
+        private UdpClient udpSender;
+        public Form1()
+        {
+            InitializeComponent();
+            thread = new Thread(new ThreadStart(olvasas));
+            thread.Start();
+        }
+
+        private async void olvasas()
+        {
+            try
+            {
+                while (fut)
+                {
+
+                    var memoryFile = MemoryMappedFile.CreateOrOpen("Local\\acpmf_physics", 784);
+                    Debug.WriteLine(memoryFile.ToString());
+                    var viewStream = memoryFile.CreateViewStream(0L, 784L);
+                    byte[] data = new byte[784];
+                    await viewStream.ReadAsync(data, 0, 784);
+                    Debug.WriteLine(data.Length);
+
+                    SharedFilePhisycs shared = new SharedFilePhisycs();
+                    var pageFileContent = shared.ByteArrayToNewStuff(data);
+                    if (pageFileContent.speedKmh == 0.0)
+                    {
+                        label2.Text = "Waiting for Assetto Corsa";
+                        Thread.Sleep(1500);
+                    }
+                    else
+                    {
+                        //label2.Text = "Assetto Corsa Online";
+                        var json = JsonConvert.SerializeObject(pageFileContent);
+
+                        IPEndPoint endpoint = new IPEndPoint(IPAddress.Broadcast, 9666);
+
+                        udpSender = new UdpClient();
+                        //udpSender.Connect(endpoint);
+                        byte[] sendableData = Encoding.ASCII.GetBytes(json);
+                        label1.Text = "UDP Broadcating is alive";
+                        await udpSender.SendAsync(sendableData, sendableData.Length, endpoint);
+                    }
+                    Thread.Sleep(100);
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            finally
+            {
+                try
+                {
+                    if (udpSender.Client.Connected)
+                    {
+                        udpSender.Close();
+                    }
+                }
+                catch (Exception exp)
+                {
+                    Debug.WriteLine(exp.Message);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (fut)
+                {
+                    fut = !fut;
+                    thread.Abort();
+                    button1.Text = "Connect to AC";
+                    label2.Text = "";
+                    label1.Text = "UDP Broadcasting turned off";
+                    try
+                    {
+                        if (udpSender.Client.Connected)
+                        {
+                            udpSender.Close();
+                        }
+                    }
+                    catch (Exception exp)
+                    {
+                        Debug.WriteLine(exp.Message);
+                    }
+                }
+                else
+                {
+                    fut = !fut;
+                    thread = new Thread(new ThreadStart(olvasas));
+                    thread.Start();
+                    button1.Text = "Stop Assetto Corsa Reading";
+                    label2.Text = "Waiting for Assetto Corsa";
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ ACTIServer/Structs/SharedFilePhisycs.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACTIServer.Structs
+{
+    public class SharedFilePhisycs
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct SPageFilePhysics
+        {
+            [MarshalAs(UnmanagedType.U4)]
+           
+            public int packetId;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float gas;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float brake;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float fuel;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float gear;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float rpms;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float steerAngle;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float speedKmh;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+            public float[] velocity;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+            public float[] accg;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            public float[] wheelSlip;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            public float[] wheelLoad;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            public float[] wheelPressure;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            public float[] wheelAngularSpeed;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            public float[] tyreWear;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            public float[] tyreDirtyLevel;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            public float[] tyreCoreTemp;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            public float[] camberRadian;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            public float[] suspesionTravel;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float drs;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float tractionControlLevel;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float heading;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float pitch;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float roll;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float cgHeight;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 5)]
+            public float[] carDamaged;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public int numberOfTyresOutFromTrack;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public int pitlimiter;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float abs;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float kersCharge;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float kersInToEngine;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public int autoShifterOn;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+            public float[] rideHeight;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float turboBoost;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float ballast;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float airDensity;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float airTemp;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float roadTemp;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+            public float[] localAngularVec;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float finalFF;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float performanceMeter;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public int engineBrake;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public int ersRecoveryLevel;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public int ersPowerLevel;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public int ersHeatCahrging;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public int erslsCharging;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float kersCurrentKJ;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public int drsAvailable;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public int drsEnabled;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            public float[] brakeTemp;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float clutch;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            public float[] tyreTempInner;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            public float[] tyreTempCenter;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            public float[] tyreTempOuter;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public int isAiControlled;
+
+            [MarshalAs(UnmanagedType.R4)]
+            public float braikeBais;
+
+        }
+
+        public SPageFilePhysics ByteArrayToNewStuff(byte[] bytes)
+        {
+            GCHandle handle = GCHandle.Alloc(bytes, GCHandleType.Pinned);
+            SPageFilePhysics stuff = (SPageFilePhysics)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(SPageFilePhysics));
+            handle.Free();
+            return stuff;
+        }
+    }
+}
